007e34e3d9b8d91da0ba721c7705102f14dd70da
akaWild <nemyx84@gmail.com>
2024-07-20 11:22:09 +0300

    Added http communication to get data from auction service on startup

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -1,7 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
-using System.Text.Json;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
@@ -19,16 +19,16 @@ namespace SearchService.Data
                 CreateAsync();
 
             var count = await DB.CountAsync<Item>();
-            if (count == 0)
-            {
-                Console.WriteLine("No data - will attempt to seed");
 
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            using var scope = app.Services.CreateScope();
 
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.WriteLine(items.Count + " returned from the auction service");
+
+            if (items.Count > 0)
                 await DB.SaveAsync(items);
-            }
         }
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,10 @@
 using SearchService.Data;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 
 var app = builder.Build();
 
