db81756d4deac2c1ff7c9e5a80ff81ae64aa7ddb
akaWild <nemyx84@gmail.com>
2024-07-22 00:07:41 +0300

    Added action card component

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,5 +1,7 @@
+import AuctionCard from "./AuctionCard";
+
 async function getData() {
-  const res = await fetch("http://localhost:6001/search");
+  const res = await fetch("http://localhost:6001/search?pageSize=10");
   if (!res.ok) throw new Error("failed to fetch data");
 
   return res.json();
@@ -7,7 +9,17 @@ async function getData() {
 
 async function Listings() {
   const data = await getData();
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {data &&
+        data.results.map((auction: any) => (
+          <AuctionCard
+            auction={auction}
+            key={auction.id}
+          />
+        ))}
+    </div>
+  );
 }
 
 export default Listings;
