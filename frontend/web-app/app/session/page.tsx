a18aea825921bf6a8e29309a858f3f41eaeeb6db
akaWild <nemyx84@gmail.com>
2024-07-23 00:57:00 +0300

    Added functionality to obtain access token

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,9 +1,10 @@
-import { getSession } from "../actions/authActions";
+import { getSession, getTokenWorkaround } from "../actions/authActions";
 import Heading from "../components/Heading";
 import AuthTest from "./AuthTest";
 
 async function Session() {
   const session = await getSession();
+  const token = await getTokenWorkaround();
 
   return (
     <div>
@@ -14,6 +15,10 @@ async function Session() {
       </div>
       <div className="mt-4">
         <AuthTest />
+      </div>
+      <div className="bg-green-200 border-2 border-blue-500 mt-4">
+        <h3 className="text-lg">Token data</h3>
+        <pre className="overflow-auto">{JSON.stringify(token, null, 2)}</pre>
       </div>
     </div>
   );
