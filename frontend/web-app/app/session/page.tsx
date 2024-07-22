2eecdf059ebbb982d2d0b0dbe0041974faaca38e
akaWild <nemyx84@gmail.com>
2024-07-23 00:42:42 +0300

    Added temp utils to test API authentication

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,5 +1,6 @@
 import { getSession } from "../actions/authActions";
 import Heading from "../components/Heading";
+import AuthTest from "./AuthTest";
 
 async function Session() {
   const session = await getSession();
@@ -10,6 +11,9 @@ async function Session() {
       <div className="bg-blue-200 border-2 border-blue-500">
         <h3 className="text-lg">Session data</h3>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+      <div className="mt-4">
+        <AuthTest />
       </div>
     </div>
   );
