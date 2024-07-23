dd0b35093429a50dca4441a970e8620eaaa760e0
akaWild <nemyx84@gmail.com>
2024-07-23 18:01:07 +0300

    Created auction form

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,5 +1,16 @@
+import Heading from "@/app/components/Heading";
+import AuctionForm from "../AuctionForm";
+
 function Create() {
-  return <div>create auction</div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subtitle="Please enter the details of your car"
+      />
+      <AuctionForm />
+    </div>
+  );
 }
 
 export default Create;
